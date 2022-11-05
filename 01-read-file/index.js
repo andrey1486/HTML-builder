343b74a37f0fc8df2d16aedf537f8236af94b7f4
andrey1486 <chervonukandrey@gmail.com>
2022-11-05 21:27:53 +0200

    refactor: 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,12 +1,7 @@
-
 const fs = require('fs');
 const path = require('path');
 const way =  path.resolve('01-read-file', 'text.txt');
-
 let readableStream = fs.createReadStream(way, 'utf8');
- 
 let data = '';
-
-
 readableStream.on('data', chunk => data+=chunk);
 readableStream.on('end', () => console.log(data));
