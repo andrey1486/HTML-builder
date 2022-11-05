8387696b2d9af5d84dde9fa24087e09e7b3e0742
andrey1486 <chervonukandrey@gmail.com>
2022-11-05 21:24:28 +0200

    add: 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -10,8 +10,3 @@ let data = '';
 
 readableStream.on('data', chunk => data+=chunk);
 readableStream.on('end', () => console.log(data));
-// fs.readFile(way, 'utf8', function (error, chunk) {
-    
-//   if (error) console.log(error); 
-//   console.log(chunk); 
-// });
