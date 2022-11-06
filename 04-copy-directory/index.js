70dcd799eab140f7985e351104b2e67d4e368a21
andrey1486 <chervonukandrey@gmail.com>
2022-11-06 15:25:37 +0200

    add:  04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,47 @@
 
+const fs = require('fs');
+const path = require('path');
+const way =  path.resolve('04-copy-directory', 'files-copy');
+const copyWay = path.resolve('04-copy-directory', 'files');
+const fsPromise = require('fs/promises');
+
+fs.mkdir(way, { recursive: true }, err => {
+  if(err) throw err;   
+});
+
+async function copyFiles(){
+  
+  try {
+    const copies = await fsPromise.readdir('04-copy-directory/files-copy',
+     {withFileTypes: true});
+    console.log(copies);
+    for (let copy of copies){
+      fs.unlink(`04-copy-directory/files-copy/${copy.name}`, function(err){
+        if (err) {
+          console.log(err);
+        } 
+        else {
+          console.log('Файл  удалённый');
+        }
+      });
+    }
+
+    const item = await fsPromise.readdir('04-copy-directory/files',
+     {withFileTypes: true});
+    for (const items of item) {
+      if (items.isFile()){
+        fs.copyFile(`04-copy-directory/files/${items.name}`,
+         `04-copy-directory/files-copy/${items.name}`, (err) => {
+          if (err) {
+            console.log('ошибка:', err);
+          }
+       });
+      }
+    }
+  } catch(err) {
+    console.log((err)); 
+  }
+
+}
+
+copyFiles();
